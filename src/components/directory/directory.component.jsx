8ef1c7c018d278bffbf58aff5fe092ef5cf84d93
Jesse <jsebaltazar@gmail.com>
2020-10-12 14:06:56 -0600

    implemented basic navigation on menu-items using react-router and withRouter HOC(Higher-Order-Compoents)

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -52,12 +52,16 @@ class Directory extends React.Component{
     render(){
             return (
                 <div className='directory-menu'> 
-                    { this.state.sections.map( ({title, imageUrl, id, size}) => (
-                            <MenuItem key={id} title={title} imgUrl={imageUrl} size={size} />
+                    { this.state.sections.map( ({id,  ...otherSectionProps}) => (
+                            <MenuItem key={id} {...otherSectionProps}/>
                         ))}
+
+                        {/* { this.state.sections.map( ({title, imageUrl, id, size, linkUrl}) => (
+                            <MenuItem key={id} title={title} imgUrl={imageUrl} size={size} linkUrl={linkUrl}/>
+                        ))}   */}
                 </div>
             );
-        }
+        } 
     }
 
     export default Directory;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 
 import '../menu-item/menu-item.styles.scss';
 
 
-const MenuItem = ({ title, imgUrl, size }) => ( 
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => ( 
 
-<div  className={`${size} menu-item`} >
-    <div className='background-image' style={{
-        backgroundImage : `url(${imgUrl})`
+<div  className={`${size} menu-item`}  onClick={ () => history.push(`${match.url}${linkUrl}`) }>
+    <div className='background-image' style={{ 
+        backgroundImage : `url(${imageUrl})`
     }}/>
     <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
@@ -19,4 +21,4 @@ const MenuItem = ({ title, imgUrl, size }) => (
 
 
 
-export default MenuItem;
+export default withRouter(MenuItem);
