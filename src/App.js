38b6a0c00d5c13e757e0388ea5c61983bbbbb47a
Jesse <jsebaltazar@gmail.com>
2020-10-17 22:01:50 -0600

    Added header component with styles and svg logo Added contact page w/ jsx and scss

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
+import ContactPage from './pages/contact/contact.component';
 
 
 
 function App() {
   return (
     <div>
+    <Header/>
      <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/contact' component={ContactPage} />
+
      </Switch>
     </div>
   );
